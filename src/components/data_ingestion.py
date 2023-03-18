30b950af717e0c831503928e7e4bc0284235ad05
VattiManojKumar <manojviru1@gmail.com>
2023-03-18 17:54:54 +0530

    Data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,6 @@
 import os
 import sys
+sys.path.insert(0, '../src')
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
@@ -51,10 +52,7 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
+    obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
-
-    modeltrainer=ModelTrainer()
-    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
